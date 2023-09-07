e1a41e5117c056d060deff8ea9aefa6bcb7c443c
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-09-08 01:07:27 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from "./Component/Nav";
+import Footer from "./Component/Footer";
+import Home from "./Component/Hero";
+
+export default function App() {
+    return (
+        <div className="bg-white">
+            <Nav />
+            <Home />
+            <Footer />
+        </div>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+import aspectRatio from "@tailwindcss/aspect-ratio";
+
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [forms, typography, aspectRatio],
+};
