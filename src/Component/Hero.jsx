fae4585a9128dbe8b10305a56d50ccf143297893
Yashasewi <Yashasewisingh2668@gmail.com>
2023-09-08 01:07:27 +0530

    Done

---
+++ src/Component/Hero.jsx
@@ -0,0 +1,15 @@
+import RelatedProducts from "./RelatedProducts";
+import Reviews from "./Reviews";
+import Product from "./Product";
+export default function Home() {
+    return (
+        <main className="mx-auto mt-8 max-w-2xl px-4 pb-16 sm:px-6 sm:pb-24 lg:max-w-7xl lg:px-8">
+            <Product />
+            {/* Reviews */}
+            <Reviews />
+
+            {/* Related products */}
+            <RelatedProducts />
+        </main>
+    );
+}
